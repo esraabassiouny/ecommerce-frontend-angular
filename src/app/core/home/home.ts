298ab9e9056cd8c89740bfcc905036b9db55b6ca
esraabassiouny <127542864+esraabassiouny@users.noreply.github.com>
2025-10-04 02:50:52 +0300

    add logo, images banner, categories slider in home

--- src/app/core/home/home.ts
+++ src/app/core/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,36 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements AfterViewInit {
 
+  @ViewChild('categoriesScroll') scrollContainer!: ElementRef;
+
+  private scrollAmount = 0;
+  private cardWidth = 220; // card width + gap
+  private scrollInterval: any;
+
+  ngAfterViewInit(): void {
+    this.startAutoScroll();
+  }
+
+  startAutoScroll(): void {
+    this.scrollInterval = setInterval(() => {
+      const container = this.scrollContainer.nativeElement;
+
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+        // Reached end → reset to start
+        container.scrollTo({ left: 0, behavior: 'smooth' });
+        this.scrollAmount = 0;
+      } else {
+        this.scrollAmount += this.cardWidth;
+        container.scrollTo({ left: this.scrollAmount, behavior: 'smooth' });
+      }
+    }, 3000); // every 3s
+  }
+
+  ngOnDestroy(): void {
+    if (this.scrollInterval) {
+      clearInterval(this.scrollInterval);
+    }
+  }
 }
